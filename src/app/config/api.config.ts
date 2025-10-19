e967fcfebd5bcaf211b4666d5e7ec4f37559fcc3
Greys2004 <gav77465@lasallebajio.edu.mx>
2025-10-18 22:36:38 -0600

    Dejar registrar asitente

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,4 @@
 // src/app/config/api.config.ts
 export const API_CONFIG = {
-  baseURL: 'https://cowboy-quizzes-silicon-icons.trycloudflare.com/VetCare/api' // No borrar /api
+  baseURL: 'https://dramatically-calculations-dna-stanley.trycloudflare.com/VetCare/api' // No borrar /api
 };
