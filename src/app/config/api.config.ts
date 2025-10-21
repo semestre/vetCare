9515ab7819e81abbfb862ac5f413d5818f4ebdfe
Greys2004 <gav77465@lasallebajio.edu.mx>
2025-10-20 19:40:43 -0600

    Arreglo en sideBar

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,4 @@
 // src/app/config/api.config.ts
 export const API_CONFIG = {
-  baseURL: 'https://diagram-set-aspects-casual.trycloudflare.com/VetCare/api' // No borrar /api
+  baseURL: 'https://nest-opening-bands-rate.trycloudflare.com/VetCare/api' // No borrar /api
 };
